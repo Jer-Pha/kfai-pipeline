4b16dc0f72fdf6db3f56c0386e011e2da887e101
Jer-Pha <131234246+Jer-Pha@users.noreply.github.com>
2025-08-15 19:32:39 -0700

    Update paths.py

--- src/kfai/core/paths.py
+++ src/kfai/core/paths.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 
 PROJECT_ROOT = Path(__file__).resolve().parent.parent.parent.parent
-print(PROJECT_ROOT)
 LOGS_DIR = PROJECT_ROOT / "logs"
 DATA_DIR = PROJECT_ROOT / "data"
 VIDEO_DATA_DIR = DATA_DIR / "video_data"
